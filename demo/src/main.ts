54a66ff3efc874f551a40c96a4c4dbc6cfac5da2
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2025-08-22 14:34:01 -0500

    lists in demo

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -4,9 +4,30 @@ import { Editor } from "@tiptap/core";
 import Document from "@tiptap/extension-document";
 import Paragraph from "@tiptap/extension-paragraph";
 import Text from "@tiptap/extension-text";
+import {
+  FlatListCore,
+  FlatListOrdered,
+  FlatListTask,
+  FlatListUnordered,
+  JoinListDOMSerializer,
+} from "../../src/";
 
-new Editor({
+const editor = new Editor({
   element: document.querySelector(".element"),
-  extensions: [Document, Paragraph, Text],
+  extensions: [
+    Document,
+    Paragraph,
+    Text,
+    FlatListCore,
+    FlatListOrdered,
+    FlatListUnordered,
+    FlatListTask,
+  ],
   content: "<p>Hello World!</p>",
 });
+
+JoinListDOMSerializer.setClipboardSerializer(editor);
+
+// To get HTML that has normal HTML lists:
+//   JoinListDOMSerializer.getHTML(editor);
+// instead of editor.getHTML().
