6c8c8e9d78446053ebe1c63ae1c5073ed4dfdd7d
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2025-08-22 14:24:26 -0500

    tiptap in demo

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -1,24 +1,12 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+import { Editor } from "@tiptap/core";
+import Document from "@tiptap/extension-document";
+import Paragraph from "@tiptap/extension-paragraph";
+import Text from "@tiptap/extension-text";
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+new Editor({
+  element: document.querySelector(".element"),
+  extensions: [Document, Paragraph, Text],
+  content: "<p>Hello World!</p>",
+});
